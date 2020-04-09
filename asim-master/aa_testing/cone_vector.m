f5c063719a4003080eedfedf2579a3d6654ee510
TigerHou2 <44105132+TigerHou2@users.noreply.github.com>
2020-04-08 22:10:30 -0500

    fixed noise application error in pathfinder navigation code
    Error was not being applied to measurements due to an unlinked variable.

--- asim-master/aa_testing/cone_vector.m
+++ asim-master/aa_testing/cone_vector.m
@@ -3,7 +3,7 @@
 close all
 clear;clc
 
-theta = repmat(deg2rad(5),3,1);
+theta = repmat(deg2rad(80),3,1);
 
 z = rand(3,1) .* (1-cos(theta)) + cos(theta);
 t = rand(3,1) .* 2*pi;
@@ -14,6 +14,6 @@ Rr = aa_getRotnMat(cross(x,tilt(1,:)'),acos(tilt(1,:)*x));
 Rv = aa_getRotnMat(cross(x,tilt(2,:)'),acos(tilt(2,:)*x));
 Ra = aa_getRotnMat(cross(x,tilt(3,:)'),acos(tilt(3,:)*x));
 
-% quiver3(zeros(3,1),zeros(3,1),zeros(3,1),tilt(:,1),tilt(:,2),tilt(:,3))
-% pbaspect([1 1 1])
-% axis equal
+quiver3(zeros(3,1),zeros(3,1),zeros(3,1),tilt(:,1),tilt(:,2),tilt(:,3))
+pbaspect([1 1 1])
+axis equal
